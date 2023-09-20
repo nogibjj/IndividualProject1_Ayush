143d8a182b8cbacf22d7aec291044532d483d40c
ayushg245 <ag758@duke.edu>
2023-09-20 11:21:53 -0400

    added

--- test_script.py
+++ test_script.py
@@ -1,12 +1,13 @@
-import stats_descriptive
+import main
 import pandas as pd
 
 data = pd.read_csv("forbes_2022_billionaires.csv")
 
+
 def test_values():
-    assert (stats_descriptive.stats_mean(data)) == 64.21068938807126
-    assert (stats_descriptive.stats_median(data)) == 64.0
-    assert (stats_descriptive.stats_std(data)) == 13.401258058138897
+    assert (main.stats_mean(data)) == 64.21068938807126
+    assert (main.stats_median(data)) == 64.0
+    assert (main.stats_std(data)) == 13.401258058138897
 
 
 test_values()
