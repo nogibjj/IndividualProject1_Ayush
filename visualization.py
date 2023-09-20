0a20aa7604ef251e95ee2fb783cf4d29d5282816
ayushg245 <143531654+ayushg245@users.noreply.github.com>
2023-09-20 11:11:22 -0400

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 
 
-df = pd.read_csv("World University Rankings 2023.csv")
+df = pd.read_csv("forbes_2022_billionaires.csv")
 
 result1= df.groupby(['country'])['finalWorth'].mean().reset_index()
 fig = px.bar(result1, x='country', y='finalWorth')
