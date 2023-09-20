4769d28395184861e67607ce21e35b4ffd7cc507
ayushg245 <143531654+ayushg245@users.noreply.github.com>
2023-09-20 11:13:56 -0400

    Delete visualization.py

--- visualization.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import plotly.express as px
-
-
-df = pd.read_csv("forbes_2022_billionaires.csv")
-
-result1= df.groupby(['country'])['finalWorth'].mean().reset_index()
-fig = px.bar(result1, x='country', y='finalWorth')
-fig.show()
